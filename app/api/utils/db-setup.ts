b637dc44191857eff0e8e345c4e9a38bb7d0dcb9
AdemolaSam <oduntanade2721@gmail.com>
2025-03-11 16:27:05 +0100

    updatr

---
+++ app/api/utils/db-setup.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+// Define an interface for the cached connection
+interface CachedConnection {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+// Declare global scope augmentation for TypeScript
+declare global {
+  var mongoose:
+    | {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+      }
+    | undefined;
+}
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable");
+}
+
+let cached: any = global.mongoose || { conn: null, promise: null };
+
+// Initialize the global cache if it doesn't exist
+if (!global.mongoose) {
+  global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose;
+}
+
+async function dbConnect(): Promise<typeof mongoose> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts: mongoose.ConnectOptions = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI as string, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
