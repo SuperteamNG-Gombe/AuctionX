27eb7259dbffc1e1e5cdfbb0dde9c2c253aef635
AdemolaSam <oduntanade2721@gmail.com>
2025-03-11 16:21:02 +0100

    removed apperror file

--- app/api/waitlist/route.ts
+++ app/api/waitlist/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
-import dbConnect from "../utils.ts/db-setup";
+import dbConnect from "../utils/db-setup";
 import { ApiError } from "next/dist/server/api-utils";
 import { addToWaitlist } from "./service";
-import { AppError } from "../utils.ts/error";
 
 export async function POST(req: Request) {
   await dbConnect();
@@ -11,12 +10,12 @@ export async function POST(req: Request) {
     const response = await addToWaitlist(body);
     return NextResponse.json({ message: response });
   } catch (error: any) {
-    if (error instanceof AppError) {
-      return NextResponse.json(
-        { error: error.message, details: error.details },
-        { status: error.statusCode }
-      );
-    }
+    // if (error instanceof AppError) {
+    //   return NextResponse.json(
+    //     { error: error.message, details: error.details },
+    //     { status: error.statusCode }
+    //   );
+    // }
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
